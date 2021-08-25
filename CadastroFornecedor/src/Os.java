f4394e065c9ce98f328f53929f9a7b17569617c6
Vitor Alves <vitornobody@gmail.com>
2021-08-25 18:04:17 -0300

    revisao

--- CadastroFornecedor/src/Os.java
+++ CadastroFornecedor/src/Os.java
@@ -3,6 +3,7 @@ import java.util.Collection;
 public class Os {
 
     private long dtInicio;
+    private Fonnecedor fornecedor;
     private Empresa empresa;
     private Collection<Servico> servicos;
     
