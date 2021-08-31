fa33dc6caacb53e7b89ae506c82e1bcad83990de
Vitor Alves <vitornobody@gmail.com>
2021-08-31 12:30:24 -0300

    User input going to local Storage

--- Interface web/main.js
+++ Interface web/main.js
@@ -1,10 +1,11 @@
-'use strict'
-
 const openModal = () => document.getElementById('modal')
     .classList.add('active')
 
-const closeModal = () => document.getElementById('modal')
-    .classList.remove('active')
+const closeModal = () => {
+    //clearFields()
+    document.getElementById('modal').classList.remove('active')
+    
+}
 
 const tempFornecedor = {
     id: "123",
@@ -38,17 +39,42 @@ const readFornecedor = () => getLocalStorage()
 //create
 const createFornecedor = (fornecedor) => {
     const dbFornecedor = getLocalStorage()
-    dbFornecedor.push (fornecedor)
+    dbFornecedor.push(fornecedor)
     setLocalStorage(dbFornecedor)
 }
 
+//Interação com layout
+const isValidFields = () => {
+    return document.getElementById('form').reportValidity()
+}
 
+const clearFields = () => {
+    const fields = document.querySelectorAll('.modal-field')
+    fields.forEach(field => field.value = "")
+}
 
+const saveFornecedor = () => {
+    if (isValidFields()) {
+        const fornecedor = {
+            nome: document.getElementById('nome').value,
+            cnpj: document.getElementById('cnpj').value,
+            tipo: document.getElementById('tipo').value,
+            contato: document.getElementById('contato').value
+        }
+        createFornecedor(fornecedor)
+        clearFields()
+        closeModal()
+    }
+}
 
+//Eventos
 document.getElementById('cadastrarFornecedor')
     .addEventListener('click', openModal)
 
 document.getElementById('modalClose')
     .addEventListener('click', closeModal)
 
-    
+document.getElementById('salvar')
+    .addEventListener('click', saveFornecedor)
+
+
