daa9119c624aa057f53ad8cdc88e1d410c8ed564
Vitor Alves <vitornobody@gmail.com>
2021-09-07 22:46:33 -0300

    All user inputs on local storage

--- Interface web/main.js
+++ Interface web/main.js
@@ -2,7 +2,7 @@ const openModal = () => document.getElementById('modal')
     .classList.add('active')
 
 const closeModal = () => {
-    //clearFields()
+    clearFields()
     document.getElementById('modal').classList.remove('active')
     enableButtons()
 }
@@ -48,15 +48,44 @@ const clearFields = () => {
 const saveFornecedor = () => {
     if (isValidFields()) {
         const fornecedor = {
-            nome: document.getElementById('nome').value,
+            nome_fantasia: document.getElementById('nome_fantasia').value,
+            razao_social: document.getElementById('razao_social').value,
+            data_cadastro: document.getElementById('data_cadastro').value,
             cnpj: document.getElementById('cnpj').value,
+            inscricao_estadual: document.getElementById('inscricao_estadual').value,
+            inscricao_municipal: document.getElementById('inscricao_municipal').value,
+            cnae: document.getElementById('cnae').value,
+            tipo_fornecedor: document.getElementById('tipo_fornecedor').value,
+            produto: document.getElementById('produto').value,
+            servico: document.getElementById('servico').value,
+
+            tipo_endereco: document.getElementById('tipo_endereco').value,
+            tipo_logradouro: document.getElementById('tipo_logradouro').value,
+            logradouro: document.getElementById('logradouro').value,
+            numero_endereco: document.getElementById('numero_endereco').value,
+            bairro: document.getElementById('bairro').value,
+            cidade: document.getElementById('cidade').value,
             uf: document.getElementById('uf').value,
-            tipo: document.getElementById('tipo').value,
-            email: document.getElementById('email').value
+            pais: document.getElementById('pais').value,
+            cep: document.getElementById('cep').value,
+            complemento: document.getElementById('complemento').value,
+
+            nome_contato: document.getElementById('nome_contato').value,
+            email_contato: document.getElementById('email_contato').value,
+            departamento_contato: document.getElementById('departamento_contato').value,
+            email_contato: document.getElementById('email_contato').value,
+            ddd_contato: document.getElementById('ddd_contato').value,
+            ddi_contato: document.getElementById('ddi_contato').value,
+            telefone_contato: document.getElementById('telefone_contato').value,
+            ramal_contato: document.getElementById('ramal_contato').value,
+            codigo_contato: document.getElementById('codigo_contato').value,
+            descricao_contato: document.getElementById('descricao_contato').value
         }
-        const index = document.getElementById('nome').dataset.index
+
+        const index = document.getElementById('nome_fantasia').dataset.index
         if (index == 'new') {
             createFornecedor(fornecedor)
+            createRow(fornecedor, index)
             clearFields()
             closeModal()
         } else {
@@ -71,11 +100,11 @@ const createRow = (fornecedor, index) => {
     const newRow = document.createElement('tr')
     newRow.innerHTML = `
         <td>${index}</td>
-        <td>${fornecedor.nome}</td>
+        <td>${fornecedor.nome_fantasia}</td>
         <td>${fornecedor.cnpj}</td>
         <td>${fornecedor.uf}</td>
-        <td>${fornecedor.tipo}</td>
-        <td>${fornecedor.email}</td>
+        <td>${fornecedor.tipo_fornecedor}</td>
+        <td>${fornecedor.email_contato}</td>
         <td>
             <div class="form-group">
                 <input class="checkbox" id="checkbox1" type='checkbox'/>
@@ -104,12 +133,39 @@ const clearTable = () => {
 
 
 const fillFields = (fornecedor) => {
-    document.getElementById('nome').value = fornecedor.nome
+    document.getElementById('nome_fantasia').value = fornecedor.nome_fantasia
+    document.getElementById('razao_social').value = fornecedor.razao_social
+    document.getElementById('data_cadastro').value = fornecedor.data_cadastro
     document.getElementById('cnpj').value = fornecedor.cnpj
+    document.getElementById('inscricao_estadual').value = fornecedor.inscricao_estadual
+    document.getElementById('inscricao_municipal').value = fornecedor.inscricao_municipal
+    document.getElementById('cnae').value = fornecedor.cnae
+    document.getElementById('tipo_fornecedor').value = fornecedor.tipo_fornecedor
+    document.getElementById('produto').value = fornecedor.produto
+    document.getElementById('servico').value = fornecedor.servico
+
+    document.getElementById('tipo_endereco').value = fornecedor.tipo_endereco
+    document.getElementById('tipo_logradouro').value = fornecedor.tipo_logradouro
+    document.getElementById('logradouro').value = fornecedor.logradouro
+    document.getElementById('numero_endereco').value = fornecedor.numero_endereco
+    document.getElementById('bairro').value = fornecedor.bairro
+    document.getElementById('cidade').value = fornecedor.cidade
     document.getElementById('uf').value = fornecedor.uf
-    document.getElementById('tipo').value = fornecedor.tipo
-    document.getElementById('email').value = fornecedor.email
-    document.getElementById('nome').dataset.index = fornecedor.index
+    document.getElementById('pais').value = fornecedor.pais
+    document.getElementById('cep').value = fornecedor.cep
+    document.getElementById('complemento').value = fornecedor.complemento
+
+    document.getElementById('nome_contato').value = fornecedor.nome_contato
+    document.getElementById('email_contato').value = fornecedor.email_contato
+    document.getElementById('departamento_contato').value = fornecedor.departamento_contato
+    document.getElementById('email_contato').value = fornecedor.email_contato
+    document.getElementById('ddd_contato').value = fornecedor.ddd_contato
+    document.getElementById('ddi_contato').value = fornecedor.ddi_contato
+    document.getElementById('telefone_contato').value = fornecedor.telefone_contato
+    document.getElementById('ramal_contato').value = fornecedor.ramal_contato
+    document.getElementById('codigo_contato').value = fornecedor.codigo_contato
+    document.getElementById('descricao_contato').value = fornecedor.descricao_contato
+    document.getElementById('nome_fantasia').dataset.index = fornecedor.index
 }
 
 const disableButtons = () => {
@@ -125,6 +181,8 @@ const enableButtons = () => {
         saveButton.hidden = false;
         cancelButton.hidden = false;
 }
+
+ 
 
 const editConsultDelete = (event) => {
     if (event.target.id == 'edit'){
@@ -145,7 +203,7 @@ const editConsultDelete = (event) => {
         console.log(fornecedor)
         fornecedor.index = index
         fillFields(fornecedor)  
-        openModal() 
+        openModal()
         disableButtons()
     }
     else if (event.target.id == "delete"){
@@ -159,6 +217,8 @@ const editConsultDelete = (event) => {
     }
 
 }
+
+updateTable()
 
 //Eventos
 document.getElementById('cadastrarFornecedor')
@@ -175,3 +235,4 @@ document.getElementById('cancelar')
 
 document.querySelector('#tableFornecedor>tbody')
     .addEventListener('click', editConsultDelete)
+
